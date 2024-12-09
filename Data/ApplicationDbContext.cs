6b38e957aff26d9a6ca452f3097058739d1112d4
rosy871 <yalameju@gmail.com>
2024-12-09 14:22:21 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Fantastic4News.Models.Db;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fantastic4News.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<User>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
---
+++ Models/Db/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fantastic4News.Models.Db
+{
+	public class User:IdentityUser
+	{
+		[StringLength(50)]
+		public required string FirstName { get; set; }
+
+		[StringLength(50)]
+		public required string LastName { get; set; }
+		public DateTime DOB { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime LastLogin { get; set; }
+
+    }
+}
