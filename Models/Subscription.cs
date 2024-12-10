adc727bf8ce59f028b4fdd5f6dd7633c816a53f4
Longh33 <lone.gharib@yahoo.se>
2024-12-10 09:38:01 +0100

    Created Models

---
+++ Models/Subscription.cs
@@ -0,0 +1,20 @@
+﻿using Fantastic4News.Models.Db;
+
+namespace Fantastic4News.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public SubscriptionType SubscriptionType { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Expired { get; set; }
+
+        public User User { get; set; }
+
+        
+    }
+}
