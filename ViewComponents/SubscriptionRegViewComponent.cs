3da560db75bd3bbf70870da6f771b39d7e85bae1
rosy871 <yalameju@gmail.com>
2024-12-16 09:39:30 +0100

    scaffold confirmemail, made viecomponent for the choosing subscription

---
+++ ViewComponents/SubscriptionRegViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Fantastic4News.Models.ViewModels;
+using Fantastic4News.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fantastic4News.ViewComponents
+{
+	public class SubscriptionRegViewComponent:ViewComponent
+	{
+        private readonly ISubscriptionService _iss;
+
+        public SubscriptionRegViewComponent(ISubscriptionService iss)
+        {
+            _iss = iss;
+            
+        }
+
+		public IViewComponentResult Invoke()
+		{
+			var subscritionstp = _iss.GetSubscriptionTypes();
+
+			return View(subscritionstp);
+		}
+	}
+}
