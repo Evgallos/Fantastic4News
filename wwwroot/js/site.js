abc504a38cc4e84b03536f96008d36f5b0d22c41
rosy871 <yalameju@gmail.com>
2024-12-17 07:55:46 +0100

    changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,60 @@
-﻿//function chooseDateTimepartial(subId) {
-//    console.log(subId);
-//    $("#chooseDate").show();
-//}
+﻿
+
+function calculateExpiresDate() {
+
+    var monthNum = parseInt($('#monthnum').val(), 10);  // Parse the month number as an integer ,10 tells that it is decimal num containin 2 nums only
+    var startDate = new Date($('#startDate').val());    // Parse the start date
+    if (monthNum == 1) {
+        $('#errmsg').text("Month value cannot be 0");
+    }
+    else if (monthNum >= 12) {
+        $('#errmsg').text("max value is a year");
+
+    }
+    else {
+
+        console.log("monthnum " + monthnum);
+        console.log("startdate " + startDate);
+
+        var expiresDate = new Date(startDate);
+        expiresDate.setMonth(startDate.getMonth() + monthNum);
+        $('#ExpiresDate').val(expiresDate);
+    }
+
+}
+
+
+
+//plus and minus month
+
+
+function minusval() {
+    var monthNum = $('#monthnum').val();
+    var startdate = $('#startDate').val();
+    if (monthNum == 1) {
+        $('#errmsg').text("Month value cannot be 0");
+    }
+    else {
+        monthNum--;
+        $('#monthnum').val(monthNum);
+    }
+}
+
+function plusval() {
+    var monthNum = $('#monthnum').val();
+    if (monthNum >= 12) {
+        $('#errmsg').text("max value is a year");
+
+    }
+    else {
+        monthNum++;
+        $('#monthnum').val(monthNum);
+    }
+}
+
+
+
+//show hide the choose subscription form
 function chooseDateTimepartial(subtpId) {
     console.log("its here" + subtpId);
     $('#forsubtyid').val(subtpId);
@@ -12,31 +65,12 @@ function cancelSubscription() {
     $('#chooseDate').hide();
 }
 
-////for other Subscription
 
 
 
-//function chooseOtherSubscription(subId) {
-//    consoe.log(subId);
-//    $.ajax({
-//        url: '/Customer/ChooseOtherSubscription',
-//        data: { id: subsId },
-//        dataType: 'json',
-
-//        success: function (data) {
-//            if (data.success) {
-//                window.location.href = data.redirectToUrl;
-//            }
-//        },
-
-//        error: function (err) {
-//            console.log(err);
-//        }
 
 
-//    });
 
-//}
 
 
 
@@ -46,7 +80,7 @@ function chooseFreeSubscription(subsId) {
     console.log(subsId);
     $.ajax({
         url: '/Customer/ChooseFreeSubscription',
-        data: { id: subsId},
+        data: { id: subsId },
         dataType: 'json',
 
         success: function (data) {
@@ -66,7 +100,7 @@ function chooseFreeSubscription(subsId) {
 // Function for like articles
 
 function likeArticle(id) {
-    
+
     $.ajax({
         type: 'post',
         url: '/Article/LikeArticle',
@@ -78,7 +112,7 @@ function likeArticle(id) {
             if (el) {
                 el.textContent = data;
             }
-            
+
         },
 
         error: function (err) {
