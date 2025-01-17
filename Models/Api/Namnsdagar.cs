446a58cd227cee524952731ccc0c7e6919a8fccc
fredrik013 <fredrik013@yahoo.se>
2025-01-17 15:51:52 +0100

    Backup 2025-01-17

---
+++ Models/Api/Namnsdagar.cs
@@ -0,0 +1,25 @@
+﻿namespace Fantastic4News.Models.Api
+{
+    public class Namnsdagar
+    {
+        public string cachetid { get; set; }
+        public string version { get; set; }
+        public string uri { get; set; }
+        public string startdatum { get; set; }
+        public string slutdatum { get; set; }
+        public Dagar[] dagar { get; set; }
+
+    }
+
+        public class Dagar
+    {
+        public string datum { get; set; }
+        public string veckodag { get; set; }
+        public string arbetsfridag { get; set; }
+        public string röddag { get; set; }
+        public string vecka { get; set; }
+        public string dagivecka { get; set; }
+        public string[] namnsdag { get; set; }
+        public string flaggdag { get; set; }
+    }
+    }
